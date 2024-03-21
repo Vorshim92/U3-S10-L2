855326c13ba712c79dd2d6f04c97f869028c71a4
Vorshim92 <vorshim92@gmail.com>
2024-03-21 17:24:33 +0100

    added ex4

--- src/test-components/Booklist.test.js
+++ src/test-components/Booklist.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import GenerateCards from "../components/BooklistComponent";
 
 const selectedGenre = 0;
@@ -7,4 +7,12 @@ test("renders booklist cards", () => {
   render(<GenerateCards selectedGenre={selectedGenre} />);
   const cardId = screen.getAllByTestId("cardtest");
   expect(cardId).toHaveLength(10);
+});
+
+test("renders booklist cards filtered by title", () => {
+  render(<GenerateCards selectedGenre={0} />);
+  const searchInput = screen.getByPlaceholderText("Cerca per titolo...");
+  fireEvent.change(searchInput, { target: { value: "A Reaper at the Gates (Ember Quartet, Book 3)" } });
+  const cardId = screen.getAllByTestId("cardtest");
+  expect(cardId).toHaveLength(1);
 });
